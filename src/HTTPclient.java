3942f240633135a12461fcf6c3cb40a69aaecbcc
LettuceFoot <febrian.francione@gmail.com>
2020-10-02 15:01:42 -0400

    testing push

--- src/HTTPclient.java
+++ src/HTTPclient.java
@@ -30,6 +30,7 @@ public class HTTPclient {
 
                     // boolean settings for the command line interface, sets to true if user enters command.
 //                    todo
+//                    test2
 //                    need to implement
                     boolean cmd_d = false;
                     boolean cmd_f = false;
