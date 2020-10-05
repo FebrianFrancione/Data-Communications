523ed775031ef85be7fd10eba2d1ec1e1a140ba4
LettuceFoot <febrian.francione@gmail.com>
2020-10-04 21:13:14 -0400

    Refactored to optimize code

--- src/HTTPclient.java
+++ src/HTTPclient.java
@@ -25,7 +25,7 @@ class Httpclient {
             //initializing string output and response
 
             String string_output;
-            String response = "";
+            StringBuilder response = new StringBuilder();
 
 //            Creating the Socket to connect
 //            using the host called and default port: 80
@@ -36,12 +36,16 @@ class Httpclient {
             BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             String file_name = null;
             //request being created
-            String cmd_request = "";
-            if (path != "" || path != null) {
-                cmd_request = "GET " + path + " HTTP/1.0";
-            } else {
+            String cmd_request;
+//            if (path.equals("") || path == null) {
+//            simplified to:
+            if (path.equals("")) {
                 cmd_request = "GET / HTTP/1.0\r\nHost: " + host + "\r\n\r\n";
+
+            } else {
+                cmd_request = "GET " + path + " HTTP/1.0";
             }
+
             System.out.println("printing command request");
             pw.println(cmd_request);
 
@@ -52,10 +56,17 @@ class Httpclient {
                 path = path.substring(0, path.indexOf("-out"));
             }
 
-            if (string_header != "") {
+//            if (string_header != "") {
+//            converted to simplified version
+            if (!string_header.equals("")) {
                 String[] header_array = string_header.split(" ");
-                for (int i = 0; i < header_array.length; i++) {
-                    pw.println(header_array[i]);
+
+//                for (int i = 0; i < header_array.length; i++) {
+//                    pw.println(header_array[i]);
+//                }
+//replaced with enhanced for loop.
+                for (String s : header_array) {
+                    pw.println(s);
                 }
 
 //String modifications
@@ -70,13 +81,15 @@ class Httpclient {
             pw.flush();
 
             while ((string_output = br.readLine()) != null) {
-                response += string_output + "\n";
+//                response += string_output + "\n";
+//                converted to append rather than +=
+                response.append(string_output).append("\n");
             }
 //            formatting the output
-                if (cmd_verbose == true) {
+                if (cmd_verbose) {
                     System.out.println(response);
                 } else {
-                    String[] formatter_response = response.split("\n\n");
+                    String[] formatter_response = response.toString().split("\n\n");
                     for (int i = 1; i < formatter_response.length; i++)
                         System.out.println(formatter_response[i]);
                 }
@@ -86,15 +99,17 @@ class Httpclient {
                     PrintWriter pw1 = new PrintWriter(file_name);
 //                    printing response
                     System.out.println("response:" +response);
-                    pw1.write(response);
+                    pw1.write(response.toString());
                     pw1.close();
                 } catch (IOException e) {
                     System.out.println(e.getMessage());
                 }
             }
-
+//closing buffered reader and printwriter instances
             br.close();
             pw.close();
+//            closing socket instance
+            System.out.println("Closing socket!");
             socket.close();
         } catch (Exception e) {
             e.printStackTrace();
@@ -142,7 +157,9 @@ class Httpclient {
             else if (file != null) {
 
                 BufferedReader br1 = new BufferedReader(new FileReader(file));
-                String line = "";
+//                shortened redundant String line = ""
+                String line;
+//                creating a new string builder instance
                 StringBuilder string_builder = new StringBuilder();
 
                 while ((line = br1.readLine()) != null) {
@@ -211,7 +228,7 @@ class Httpclient {
 
     }
 
-    //Displays the help menu
+    //Displays the help menu, called in command line
     public static void cmd_help(String cmd_help_option) {
         if (cmd_help_option.equals("get")) {
             System.out.println(
@@ -236,6 +253,7 @@ class Httpclient {
                             + "Either [-d] or [-f] can be used but not both."
             );
         } else{
+//            help option called to "help" is useless but enters else section
             System.out.println("httpc is a curl-like application but supports HTTP protocol only.\n"
                     + "Usage:\n"
                     + "\t httpc command [arguments]\n"
@@ -250,8 +268,8 @@ class Httpclient {
 
     public static void main(String[] args) {
         String value;
-        Console console = System.console();
-        if (console == null) {
+        Console command_console = System.console();
+        if (command_console == null) {
             System.out.println("Enter your terminal to be able to access the command line");
             System.out.println("Basic troubleshooting: In terminal, enter the cd src directory, then enter \" Javac HTTPclient.java \" and finally enter \" Java Httpclient\". This will allow you to access the rest of the program.");
             try {
@@ -263,37 +281,34 @@ class Httpclient {
         }
         System.out.println("Assignment 1: Febrian Francione 40049253, ");
         //HTTP client
-        while (patternCheck != true) {
+        while (!patternCheck) {
             System.out.println("Command Line Menu");
             System.out.println("Enter \"1\" to open help options");
             System.out.println("Enter \"2\" to open the httpc functions");
-            String option = console.readLine();
+            String option = command_console.readLine();
 //            Menu functions
             if (option.equals("1")) {
                 System.out.println("Enter \"help\" for default help");
                 System.out.println("Enter \"get\" for get help");
                 System.out.println("Enter \"post\" for post help");
                 System.out.println("Enter anything else to return to the main menu");
-                String cmd_help_option = console.readLine();
+                String cmd_help_option = command_console.readLine();
 
                 if(cmd_help_option.equals("help") || cmd_help_option.equals("get") || cmd_help_option.equals("post")) {
                     System.out.println("You have entered: " + cmd_help_option);
 //                    calling the cmd_help method to return appropriate response based off user's input
                     cmd_help(cmd_help_option);
                     System.out.println("Return to Main Menu? (Enter any key)");
-                    console.readLine();
-                }
-                else {
-                    continue;
+                    command_console.readLine();
                 }
             } else if(option.equals("2")) {
-                value = console.readLine("Enter HTTP function (Enter \"exit\" to return to Command Line Menu): ");
+                value = command_console.readLine("Enter HTTP function (Enter \"exit\" to return to Command Line Menu): ");
                 //Exit if the value entered is 0
                 if (value.equals("0")) {
                     continue;
                 }
                 //separate entities grouped within parenthesis. Seperating with regex to be worked on in following if loops
-                Pattern http_pattern = Pattern.compile("httpc(\\s+(get|post))((\\s+-v)?(\\s+-h\\s+([^\\s]+))?(\\s+-d\\s+('.+'))?(\\s+-f\\s+([^\\s]+))?)(\\s+'((http[s]?:\\/\\/www\\.|http[s]?:\\/\\/|www\\.)?([^\\/]+)(\\/.+)?)'*)");
+                Pattern http_pattern = Pattern.compile("httpc(\\s+(get|post))((\\s+-v)?(\\s+-h\\s+([^\\s]+))?(\\s+-d\\s+('.+'))?(\\s+-f\\s+([^\\s]+))?)(\\s+'((http[s]?:\\/\\/www\\.|http[s]?:\\/\\/|www\\.)?([^/]+)(\\/.+)?)'*)");
                 Matcher matcher = http_pattern.matcher(value);
                 if (matcher.find()) {
                     patternCheck = true;
@@ -354,7 +369,7 @@ class Httpclient {
                     }
                     System.out.println("User input has been processed, sending info to httpc");
 //                    calling the http client method, sending the now processed parameters.
-                    httpc_client_method(path, host, match_type, cmd_data, cmd_file, cmd_verbose, file_name);
+                    HTTPC_client_method(path, host, match_type, cmd_data, file_name);
                 } else {
                     System.out.println("Invalid Input!");
                 }
@@ -368,7 +383,7 @@ class Httpclient {
 
 
 
-    public static void httpc_client_method(String path, String host, String type, boolean isData, boolean isFile, boolean isVerbose, File file) {
+    public static void HTTPC_client_method(String path, String host, String type, boolean isData, File file) {
         try {
             if (host == null || host.equals("")) {
                 host = "www.httbin.org";
