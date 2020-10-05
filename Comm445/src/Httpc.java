f02687595b66aa29903de5752bc5a876452e270f
Marjanaupama <wyzhip-gygvyg-7sasMe>
2020-10-04 23:23:44 -0400

    Final changes everything works

--- Comm445/src/Httpc.java
+++ Comm445/src/Httpc.java
@@ -4,25 +4,30 @@ public class Httpc {
 
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
-	
-			//just testing for now
 			RequestLibrary lib = new RequestLibrary();
-			lib.post("httpc post -h Content-Type:application/json -d '{\"Assignment\": 1}' http://httpbin.org/post");
-			//String entry = " ";
-			//Scanner scan = new Scanner(System.in);
-			//RequestLibrary lib = new RequestLibrary();
-			/*System.out.println("What command do you want to use: ");
+			String entry = " ";
+			String cmd;
+			int stop = 0;
+			Scanner scan = new Scanner(System.in);
+			System.out.println("What command do you want to use: ");
 			entry = scan.nextLine();
 			while(!entry.contains("Close")) {
-				if(entry.contains("get")) {
-					lib.get(entry);
-					if(entry.contains("-v")) {
-						lib.getWVerbose(entry);
-					}
+				if(entry.contains("get -v")) {
+					cmd = entry.substring(6);
+					lib.getWVerbose(cmd);
+					entry = scan.nextLine();
 				}
-				else {
+				else if(entry.contains("get")) {
+					cmd = entry.substring(3);
+					lib.get(cmd);
+					entry = scan.nextLine();
+				}
+				else if(entry.contains("post")){
 					lib.post(entry);
+					entry = scan.nextLine();
 				}
-			}*/
+				System.out.println("Connection Stopped");
+				entry = "Close";
+			}
 	}
 }
