b09217abff9799c2c380b28c0aa039e672471421
Marjanaupama <wyzhip-gygvyg-7sasMe>
2020-10-25 08:11:19 -0400

    Refactoring

--- Comm445/src/Httpc.java
+++ Comm445/src/Httpc.java
@@ -1,30 +1,38 @@
 import java.util.Scanner;
+import java.net.URI;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Httpc {
 
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 			RequestLibrary lib = new RequestLibrary();
+			Pattern pattern;
+		    Matcher matcher;
 			String entry = " ";
 			String cmd;
 			int stop = 0;
+			boolean match;
+			
 			Scanner scan = new Scanner(System.in);
 			System.out.println("What command do you want to use: ");
 			entry = scan.nextLine();
 			while(!entry.contains("Close")) {
-				if(entry.contains("get -v")) {
-					cmd = entry.substring(6);
-					lib.getWVerbose(cmd);
-					entry = scan.nextLine();
+				//lib.get("get http://httpbin.org/get?course=networking&assignment=1");
+				if(entry.contains("get ")) {
+					pattern = Pattern.compile(".*-v.*");
+					matcher = pattern.matcher(entry);
+					if(matcher.find() == true) {
+						lib.getWVerbose(entry);
+					}
+					else{
+						lib.get(entry);
+					}
 				}
-				else if(entry.contains("get")) {
-					cmd = entry.substring(3);
-					lib.get(cmd);
-					entry = scan.nextLine();
-				}
-				else if(entry.contains("post")){
-					lib.post(entry);
-					entry = scan.nextLine();
+				//for post
+				else {
+					
 				}
 				System.out.println("Connection Stopped");
 				entry = "Close";
