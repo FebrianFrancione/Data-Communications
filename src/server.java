da25822dd2d0be4271267f50a0124444978a45b3
LettuceFoot <febrian.francione@gmail.com>
2020-10-29 14:10:18 -0400

    Added more functionalities to server file, needs to be shortened and re-organized.

--- src/server.java
+++ src/server.java
@@ -1,16 +1,33 @@
 import com.sun.net.httpserver.HttpServer;
 
 import java.io.*;
-import java.io.
 import java.net.*;
 import java.time.*;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Date;
 
+class HeaderRequestException extends Exception{
+    public HeaderRequestException(String message){
+        super(message);
+    }
+}
+//
+//class HttpRequestFormatException extends Exception {
+//    public HttpRequestFormatException(String message) {
+//        super(message);
+//    }
+//}
+
+interface HttpRequestHandler {
+    ResponseLibrary handleRequest(RequestLibrary httpRequest);
+
+}
+
 //server used for base for httpfs
 public class server{
 
+    private HttpRequestHandler requestHandler;
     private boolean debug;
     private int port;
     public static final String HTTP_VERSION = "HTTP/1.0";
@@ -34,64 +51,71 @@ public class server{
                 }
 
                 try{
-                    http_request = extractRequerst(in);
+//                    new RequestLibrary(method, requestURI, httpVersion, entityBody, content_length);
+                    http_request = extractRequest(in);
 //if(debug) System.out.println("Request: " + http_request);
                     //todo
                     //do extract and enter the 6 vaariables
-                    ResponseLibrary handle = new ResponseLibrary(http_request);
-                    http_response = ;
+                    http_response = requestHandler.handleRequest(http_request);
+//                    http_response = http_request.getMethod(),http_request.getRequest(),http_request.getHttp_version(),http_request.getBody(),http_request.getContent_length()
+                }catch (HeaderRequestException e){
+                    http_response = error_response(ResponseLibrary.status_500, e.getMessage());
                 }
+                // need to seperate erros to get a secodn error for400 bad request
+                out.print(http_response.toString());
+                out.flush();
 
             }
         }
 
     }
 
-    private RequestLibrary extractRequest(BufferedReader in) throws Exception{
+    //    private RequestLibrary extractRequest(BufferedReader in) throws HttpRequestFormatException, HeaderIOException{
+    private RequestLibrary extractRequest(BufferedReader in) throws HeaderRequestException{
         ArrayList<String> headerLines;
         try{
+            boolean justReadCRLF = false;
+            boolean cr = false;
+            int fromServer;
+            char currentChar;
+            StringBuilder sb = new StringBuilder();
+            ArrayList<String> header =new ArrayList<>();
+
+            while((fromServer = in.read()) != -1){
+                currentChar = (char)fromServer;
+
+                if(cr && currentChar == '\n'){
+                    //CRLF
+                    header.add(sb.toString());
+                    sb =new StringBuilder();
+                    if(justReadCRLF){
+                        break;
+                    }
+                    cr = false;
+                    justReadCRLF = true;
+                }else if(currentChar == '\r'){
+                    if(cr){
+                        justReadCRLF = false; // two cr'sbreaks crlf crl;f sequence
+                    }else cr = true;
+                }else{
+                    cr=false;
+                    justReadCRLF = false;
+                    sb.append(currentChar);
+                }
+            }
+            headerLines = header;
 //            headerLines = getHeader(in);
-//            boolean justReadCRLF = false;
-//            boolean cr = false;
-//            int fromServer;
-//            char currentChar;
-//            StringBuilder sb = new StringBuilder();
-//            ArrayList<String> header =new ArrayList<>();
-//
-//            while((fromServer = in.read()) != -1){
-//                currentChar = (char)fromServer;
-//
-//                if(cr && currentChar == '\n'){
-//                    //CRLF
-//                    header.add(sb.toString());
-//                    sb =new StringBuilder();
-//                    if(justReadCRLF){
-//                        break;
-//                    }
-//                    cr = false;
-//                    justReadCRLF = true;
-//                }else if(currentChar == '\r'){
-//                    if(cr){
-//                        justReadCRLF = false; // two cr'sbreaks crlf crl;f sequence
-//                    }else cr = true;
-//                }else{
-//                    cr=false;
-//                    justReadCRLF = false;
-//                    sb.append(currentChar);
-//                }
-//            }
-//            headerLines = header;
-            headerLines = getHeader(in);
-        }catch(Exception e){
+        }catch(IOException e){
             System.out.println("Could not extract HTTP header");
-            e.printStackTrace();
+            throw new HeaderRequestException("Request Header is ill-formed\n");
         }
         // parse header lines
-        if(headerLines.isEmpty()) throw new Exception("fdqw");
+        if(headerLines.isEmpty()) throw new HeaderRequestException("Request Header is ill-formed\n");
+
 
         String[]requestLineArgs = headerLines.get(0).split("\\s+");
         if(requestLineArgs.length != 3){
-            throw new Exception("request ill formed (three header objects required): " + headerLines.get(0) + "\n");
+            throw new HeaderRequestException("request ill formed (three header objects required): " + headerLines.get(0) + "\n");
         }
 
         String method = requestLineArgs[0];
@@ -101,14 +125,14 @@ public class server{
         //todo
         //replace with sout
         if(!httpVersion.equalsIgnoreCase(HTTP_VERSION)){
-            throw new Exception("Unsupported version: " + httpVersion);
+            throw new HeaderRequestException("Unsupported version: " + httpVersion);
         }
         if(!requestURI.matches("^/.*")){
-            throw new Exception("Wrong format for URI path: " + requestURI);
+            throw new HeaderRequestException("Wrong format for URI path: " + requestURI);
         }
 
         if(!method.equalsIgnoreCase(RequestLibrary.GET) && !method.equalsIgnoreCase(RequestLibrary.POST)){
-            throw new Exception("wrong mehtod: " + method);
+            throw new HeaderRequestException("wrong mehtod: " + method);
         }
 
         //parsing header lines
@@ -127,79 +151,90 @@ public class server{
         }
 
         if(!contentLengthisSet && method.equalsIgnoreCase(RequestLibrary.POST)){
-            throw new Exception("No content length for POST");
+            throw new HeaderRequestException("No content length for POST");
         }
 
         String entityBody = null;
         if (content_length > 0) {
             try {
-                entityBody = getBody(in, content_length);
+//                entityBody = getBody(in, content_length);
+                int currentChar;
+                int byteCount = 0;
+                StringBuilder sb = new StringBuilder();
+
+                while (byteCount < content_length && (currentChar = in.read()) != -1) {
+
+                    // read() gives an int from 0 to 65535
+                    // We need to know whether the character is a 1-byte, or 2-byte character
+                    byteCount += (currentChar > 127)? 2 : 1;
+                    sb.append((char)currentChar);
+                }
+
+                entityBody =  sb.toString();
             } catch (IOException e) {
-                throw new Exception("Problem reading the entity body.\n");
+                throw new HeaderRequestException("Problem reading the entity body.\n");
             }
         }
-
-//        return new ResponseLibrary(HTTP_VERSION, status, date.format(ZonedDateTime.now()), null, message, content_length);
-        return RequestLibrary(method,requestURI, httpVersion, content_length, entityBody,);
+        return new RequestLibrary(method, requestURI, httpVersion, entityBody, content_length);
     }
 
 
-    private String getBody(BufferedReader in, int size) throws  IOException{
+//    private String getBody(BufferedReader in, int size) throws  IOException{
+//
+//        int currentChar;
+//        int byteCount = 0;
+//        StringBuilder sb = new StringBuilder();
+//
+//        while (byteCount < size && (currentChar = in.read()) != -1) {
+//
+//            // read() gives an int from 0 to 65535
+//            // We need to know whether the character is a 1-byte, or 2-byte character
+//            byteCount += (currentChar > 127)? 2 : 1;
+//            sb.append((char)currentChar);
+//        }
+//
+//        return sb.toString();
+//    }
 
-        int currentChar;
-        int byteCount = 0;
-        StringBuilder sb = new StringBuilder();
-
-        while (byteCount < size && (currentChar = in.read()) != -1) {
-
-            // read() gives an int from 0 to 65535
-            // We need to know whether the character is a 1-byte, or 2-byte character
-            byteCount += (currentChar > 127)? 2 : 1;
-            sb.append((char)currentChar);
-        }
-
-        return sb.toString();
-    }
-
-    private ArrayList<String> getHeader(BufferedReader in) throws IOException {
-
-        ArrayList<String> header = new ArrayList<>();
-        boolean cr = false;
-        boolean justReadCRLF = false;
-        int fromServer;
-        char currentChar;
-        StringBuilder sb = new StringBuilder();
-
-        //add the header lines, separated by CRLF
-        while ((fromServer = in.read()) != -1) {
-
-            currentChar = (char)fromServer;
-
-            if (cr && currentChar == '\n') { // crlf
-                header.add(sb.toString());
-                sb = new StringBuilder();
-
-                if (justReadCRLF) { //two crlf in a row = end of header lines
-                    break;
-                }
-                cr = false;
-                justReadCRLF = true;
-            }
-            else if (currentChar == '\r') {
-                if (cr) {
-                    justReadCRLF = false; // two cr's in a row breaks a possible crlfcrlf sequence
-                }
-                else cr = true;
-            }
-            else {
-                cr = false;
-                justReadCRLF = false;
-                sb.append(currentChar);
-            }
-        }
-
-        return header;
-    }
+//    private ArrayList<String> getHeader(BufferedReader in) throws IOException {
+//
+//        ArrayList<String> header = new ArrayList<>();
+//        boolean cr = false;
+//        boolean justReadCRLF = false;
+//        int fromServer;
+//        char currentChar;
+//        StringBuilder sb = new StringBuilder();
+//
+//        //add the header lines, separated by CRLF
+//        while ((fromServer = in.read()) != -1) {
+//
+//            currentChar = (char)fromServer;
+//
+//            if (cr && currentChar == '\n') { // crlf
+//                header.add(sb.toString());
+//                sb = new StringBuilder();
+//
+//                if (justReadCRLF) { //two crlf in a row = end of header lines
+//                    break;
+//                }
+//                cr = false;
+//                justReadCRLF = true;
+//            }
+//            else if (currentChar == '\r') {
+//                if (cr) {
+//                    justReadCRLF = false; // two cr's in a row breaks a possible crlfcrlf sequence
+//                }
+//                else cr = true;
+//            }
+//            else {
+//                cr = false;
+//                justReadCRLF = false;
+//                sb.append(currentChar);
+//            }
+//        }
+//
+//        return header;
+//    }
 
     public static ResponseLibrary error_response(String status, String message){
         int content_length = message.getBytes().length;
